70a6a0e75981e525ca094db22ed88d64cb7fe9fc
Valeriy <binder_z@mail.ru>
2016-04-04 16:50:10 +0400

    BoardConfig: add copyright, selinux permissive, change kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2016 Nitrogen Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 TARGET_OTA_ASSERT_DEVICE := sprout,sprout4
 
 TARGET_RECOVERY_FSTAB := device/google/sprout4/rootdir/root/fstab.sprout
@@ -28,8 +44,8 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 # kernel stuff
 TARGET_KERNEL_SOURCE := kernel/mediatek/sprout
-TARGET_KERNEL_CONFIG := cyanogenmod_sprout_defconfig
-BOARD_KERNEL_CMDLINE :=
+TARGET_KERNEL_CONFIG := sprout_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
